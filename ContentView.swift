b7674a05f4a81972e9d549bf0ed3e89f4e099028
Zahra Babayeva <zbabayeva9026@stu.d214.org>
2026-04-13 11:05:48 -0500

    aded code

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var answer = 0
+    @State var sgowerror: Bool = false
     var body: some View {
+    
         Text("Andrew")
+     
         Text("Zahra")
+        Button(action: {
+            answer = (number1 * number2)
+            
+        },label: {
+            Text("Got Answer ")
+                .frame(width: 400,height: 30,alignment: .center)
+                .font(.largeTitle)
+                .background(.red)
+                .foregroundStyle(.white)
+        })
     }
 }
