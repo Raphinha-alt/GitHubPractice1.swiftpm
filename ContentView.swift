e66e0038186d891a71ff99bb2de23003189386f0
AndrewZ <p075908912@gmail.com>
2026-04-13 11:14:22 -0500

    fixxed code

--- ContentView.swift
+++ ContentView.swift
@@ -5,7 +5,7 @@ struct ContentView: View {
     @State var number2 = 0
     @State var answer = 0
     var body: some View {
-    
+        
         
         Text("Andrew")
         Text("Zahra")
@@ -22,13 +22,13 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
             
             Text("=")
+            Text("\(answer)")
+        }
         Button(action: {
             answer = (number1 * number2)
             
-            Text("\(answer)")
         }
-        
-        },label: {
+               ,label: {
             Text("Got Answer ")
                 .frame(width: 400,height: 30,alignment: .center)
                 .font(.largeTitle)
