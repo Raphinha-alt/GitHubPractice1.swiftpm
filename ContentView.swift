59cfd88aab130119cf4470da1e2ecf45c3aa02ac
AndrewZ <p075908912@gmail.com>
2026-04-13 11:05:32 -0500

    added code

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1 = 0
+    @State var number2 = 0
+    @State var answer = 0
     var body: some View {
+        
         Text("Andrew")
         Text("Zahra")
+        HStack{
+            TextField("#", value: $number1, format: .number)
+                .frame(width: 40)
+                .textFieldStyle(.roundedBorder)
+                .multilineTextAlignment(.center)
+            Text("*")
+            
+            TextField("#", value: $number2, format: .number)
+                .frame(width: 40)
+                .textFieldStyle(.roundedBorder)
+                .multilineTextAlignment(.center)
+            
+            Text("=")
+            
+            Text("\(answer)")
+        }
+        
     }
 }
